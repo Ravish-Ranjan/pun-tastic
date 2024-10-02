e1d1ac102076e0d1821f182a67288af3ee9060de
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-10-02 20:48:52 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [err, setErr] = useState(null);
+
+    const fetchData = async () => {
+        setLoading(true);
+        setErr(null);
+        try {
+            const res = await axios.get("https://punapi.rest/api/pun", {
+                withCredentials: true,
+            });
+            console.log(res);
+
+            setData(res.data);
+        } catch (error) {
+            setErr(error.message);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    if (loading) return <div>loading</div>;
+    if (err) return <div>err : {err}</div>;
+
+    return <div>{data}</div>;
 }
 
 export default App;

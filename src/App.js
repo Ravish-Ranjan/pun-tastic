81269bd2ceef19de9f74f9bfeac42576ff6637f0
Ravish Ranjan <122588009+Ravish-Ranjan@users.noreply.github.com>
2024-10-02 21:15:38 +0530

    v1 styling

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import { useEffect, useState } from "react";
 
 function App() {
     const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [err, setErr] = useState(null);
 
     const fetchData = async () => {
         setLoading(true);
         setErr(null);
         try {
-            const res = await axios.get("https://punapi.rest/api/pun", {
+            const res = await axios.get("/api/pun", {
                 withCredentials: true,
             });
-            console.log(res);
-
             setData(res.data);
         } catch (error) {
             setErr(error.message);
@@ -25,12 +23,12 @@ function App() {
 
     useEffect(() => {
         fetchData();
-    }, []);
+    }, [err]);
 
-    if (loading) return <div>loading</div>;
-    if (err) return <div>err : {err}</div>;
+    if (loading) return <div className="board">loading</div>;
+    if (err) return <div className="board">err : {err}</div>;
 
-    return <div>{data}</div>;
+    return <div className="board text-2xl w-3/4 text-center xl:text-4xl" >{data.pun}</div>;
 }
 
 export default App;
